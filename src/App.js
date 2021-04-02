9dd28ffabc0513a4c93db472ce03377d9d62d83e
coseo12 <momo_12@naver.com>
2021-04-02 17:02:04 +0900

    Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import { useReactiveVar } from '@apollo/client';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { isLoggedInVar } from './apollo';
+import { ThemeProvider } from 'styled-components';
+import { isLoggedInVar, darkModeVar } from './apollo';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import NotFound from './screens/NotFound';
+import { darkTheme, GlobalStyles, lightTheme } from './styles';
 
 const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { isLoggedInVar } from '../apollo';
+import styled from 'styled-components';
+import { darkModeVar } from '../apollo';
+// import { isLoggedInVar } from '../apollo';
+
+const Title = styled.h1`
+  color: ${props => props.theme.fontColor};
+`;
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
 
 const Login = () => {
-  const login = () => {
-    isLoggedInVar(true);
-  };
+  // const login = () => {
+  //   isLoggedInVar(true);
+  // };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={login}>Log in now!</button>
-    </div>
+    <Container>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>to dark!</button>
+      <button onClick={() => darkModeVar(false)}>to light!</button>
+    </Container>
   );
 };
 
