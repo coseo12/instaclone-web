13814269a3de29d80ec6ff7b08aedd45760fd73d
coseo12 <momo_12@naver.com>
2021-04-03 20:18:44 +0900

    Login UI Clone

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,26 +1,127 @@
+import {
+  faFacebookSquare,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
-import { darkModeVar } from '../apollo';
-// import { isLoggedInVar } from '../apollo';
-
-const Title = styled.h1`
-  color: ${props => props.theme.fontColor};
-`;
+import { WhiteBox } from '../styles';
 
 const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const TopBox = styled(WhiteBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+  form {
+    margin-top: 35px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    input {
+      width: 100%;
+      border-radius: 3px;
+      padding: 7px;
+      background-color: #fafafa;
+      border: 0.5px solid rgb(219, 219, 219);
+      margin-top: 5px;
+      box-sizing: border-box;
+      &::placeholder {
+        font-size: 12px;
+      }
+      &:last-child {
+        border: none;
+        margin-top: 12px;
+        background-color: #0095f6;
+        color: white;
+        text-align: center;
+        padding: 6px 0px;
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+const BottomBox = styled(WhiteBox)`
+  padding: 20px 0px;
+  text-align: center;
+  a {
+    font-weight: 600;
+    color: #0095f6;
+  }
+`;
+
+const Wrapper = styled.div`
+  max-width: 350px;
+  width: 100%;
+`;
+
+const Separator = styled.div`
+  margin: 20px 0px 30px 0px;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  div {
+    width: 100%;
+    height: 2px;
+    background-color: rgb(219, 219, 219);
+  }
+  span {
+    margin: 0px 10px;
+    color: #8e8e8e;
+    font-weight: 600;
+  }
+`;
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+  }
 `;
 
 const Login = () => {
-  // const login = () => {
-  //   isLoggedInVar(true);
-  // };
-
   return (
     <Container>
-      <Title>Login</Title>
-      <button onClick={() => darkModeVar(true)}>to dark!</button>
-      <button onClick={() => darkModeVar(false)}>to light!</button>
+      <Wrapper>
+        <TopBox>
+          <div>
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
+          </div>
+          <form>
+            <input type="text" name="username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="submit" value="Log In" />
+          </form>
+          <Separator>
+            <div></div>
+            <span>OR</span>
+            <div></div>
+          </Separator>
+          <FacebookLogin>
+            <FontAwesomeIcon icon={faFacebookSquare} />
+            <span>Log in with Facebook</span>
+          </FacebookLogin>
+        </TopBox>
+        <BottomBox>
+          <span>Don't have an account?</span>
+          <a href="#test"> Sign up</a>
+        </BottomBox>
+      </Wrapper>
     </Container>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+
+export const WhiteBox = styled.div`
+  background-color: white;
+  border: 1px solid rgb(219, 219, 219);
+  width: 100%;
+`;
 
 export const lightTheme = {
   fontColor: '#2c2c2c',
@@ -13,7 +19,18 @@ export const darkTheme = {
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
+    * {
+      box-sizing: border-box;
+    }
+    input {
+      all: unset;
+    }
     body {
-        background-color: ${props => props.theme.bgColor};
+        background-color:#FAFAFA;
+        font-size: 14px;
+        font-family: 'Open Sans', sans-serif;
+    }
+    a {
+      text-decoration: none;
     }
 `;
