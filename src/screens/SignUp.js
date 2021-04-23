baa1c714d6583359682e7ad9ac22022f541860e1
coseo12 <momo_12@naver.com>
2021-04-23 16:47:59 +0900

    create account

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -11,6 +11,11 @@ import Separator from '../components/auth/Separator';
 import { FatLink } from '../components/shared/styled';
 import routes from '../routes';
 import PageTitle from '../components/shared/PageTitle';
+import { useForm } from 'react-hook-form';
+import FormError from '../components/auth/FormError';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import { useHistory } from 'react-router';
 
 const HedaderContainer = styled.div`
   display: flex;
@@ -24,7 +29,75 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String!
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 const SignUp = () => {
+  const history = useHistory();
+  const {
+    register,
+    errors,
+    formState,
+    handleSubmit,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const onCompleted = data => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return setError('result', { message: error });
+    }
+    history.push(routes.home);
+  };
+
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+
+  const onSubmitValid = () => {
+    if (loading) {
+      return;
+    }
+    const { firstName, lastName, email, username, password } = getValues();
+    createAccount({
+      variables: {
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+      },
+    });
+  };
+
+  const clearLoginError = () => {
+    clearErrors('result');
+  };
+
   return (
     <AuthLayout>
       <PageTitle title="Sign up" />
@@ -36,12 +109,51 @@ const SignUp = () => {
           </Subtitle>
         </HedaderContainer>
         <Separator />
-        <form>
-          <Input type="text" name="name" placeholder="Name" />
-          <Input type="text" name="email" placeholder="Email" />
-          <Input type="text" name="username" placeholder="Username" />
-          <Input type="password" name="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({ required: 'First name is required' })}
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={clearLoginError}
+          />
+          <FormError message={errors.firstName?.message} />
+          <Input
+            ref={register}
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+          />
+          <Input
+            ref={register({ required: 'Email is required' })}
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={clearLoginError}
+          />
+          <FormError message={errors.email?.message} />
+          <Input
+            ref={register({ required: 'Username is required' })}
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={clearLoginError}
+          />
+          <FormError message={errors.username?.message} />
+          <Input
+            ref={register({ required: 'Password is required' })}
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={clearLoginError}
+          />
+          <FormError message={errors.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? 'Loading...' : 'Sign up'}
+            disabled={!formState.isValid || loading}
+          />
+          <FormError message={errors.result?.message} />
         </form>
       </FormBox>
       <BottomBox
