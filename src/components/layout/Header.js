8c6a89d05fb20ea68a30b3b13e1183bd4f3bc158
coseo12 <momo_12@naver.com>
2021-04-27 16:18:20 +0900

    Header setup

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,11 @@ import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faCompass, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { isLoggedInVar } from '../../apollo';
+import useUser from '../../hooks/useUser';
+import routes from '../../routes';
 
 const SHeader = styled.header`
   width: 100%;
@@ -31,8 +34,18 @@ const Icon = styled.span`
   margin-left: 15px;
 `;
 
+const Button = styled.span`
+  background-color: ${props => props.theme.accent};
+  border-radius: 4px;
+  padding: 5px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 function Header() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  useUser();
+
   return (
     <SHeader>
       <Wrapper>
@@ -52,7 +65,11 @@ function Header() {
                 <FontAwesomeIcon icon={faUser} size="lg" />
               </Icon>
             </>
-          ) : null}
+          ) : (
+            <Link href={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </Column>
       </Wrapper>
     </SHeader>
