bd3a6b4d069dc74c121e108f4d8996746659ff2e
coseo12 <momo_12@naver.com>
2021-04-28 17:30:12 +0900

    photo part 1

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,57 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import React from 'react';
-import { logUserOut } from '../apollo';
+import styled from 'styled-components';
+import Avatar from '../components/shared/Avatar';
+import { FatText } from '../components/shared/styled';
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${props => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
 
 const Home = () => {
-  const logout = () => {
-    logUserOut();
-  };
+  const { data } = useQuery(FEED_QUERY);
 
   return (
     <div>
-      <h1>Home</h1>
-      <button onClick={logout}>Log out now!</button>
+      {data?.seeFeed?.map(photo => (
+        <PhotoContainer>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 };
